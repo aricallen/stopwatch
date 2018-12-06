a27cf34b0cc268ed611f09f8812088dcb7ec4bbd
aricallen <aric.allen2@gmail.com>
2018-12-06 08:35:06 -0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+export enum StopwatchState {
+  RUNNING = 'RUNNING',
+  IDLE = 'IDLE'
+}
+
+export interface StopwatchRecord {
+  state: StopwatchState;
+  startTime: number | null;
+}
+
+export interface IStopwatch {
+  start(): void;
+  stop(): void;
+  reset(): void;
+  getDuration(): number | null;
+  getState(): StopwatchState;
+  getStartTime(): number | null;
+  isRunning(): boolean;
+  toRecord(): StopwatchRecord;
+  // static fromRecord(StopwatchRecord): IStopwatch
+}
+
+class Stopwatch implements IStopwatch {
+  private state: StopwatchState;
+  private startTime: number | null;
+
+  constructor() {
+    this.state = StopwatchState.IDLE;
+    this.startTime = null;
+  }
+
+  start() {
+    this.state = StopwatchState.RUNNING;
+    this.startTime = Date.now();
+  }
+
+  stop() {
+    this.state = StopwatchState.IDLE;
+  }
+
+  reset() {
+    this.startTime = null;
+  }
+
+  getDuration() {
+    if (this.startTime) {
+      return Date.now() - this.startTime;
+    }
+    return null;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  getStartTime() {
+    return this.startTime;
+  }
+
+  isRunning() {
+    return this.state === StopwatchState.RUNNING;
+  }
+
+  toRecord() {
+    return {
+      state: this.state,
+      startTime: this.startTime,
+    };
+  }
+
+  static fromRecord({ stopwatchRecord }: { stopwatchRecord: StopwatchRecord }): IStopwatch {
+    return Object.assign(new Stopwatch(), stopwatchRecord);
+  }
+}
+
+export { Stopwatch };
---
+++ tests/stopwatch.js
@@ -0,0 +1,46 @@
+const test = require('tape');
+const { Stopwatch, StopwatchState } = require('../dist/index.js');
+
+test(`start`, (t) => {
+  const sw = new Stopwatch();
+  sw.start();
+  t.equal(sw.getState(), StopwatchState.RUNNING);
+  t.equal(sw.isRunning(), true);
+  t.notEqual(sw.getDuration(), null);
+  t.end();
+});
+
+test(`stop`, (t) => {
+  const sw = new Stopwatch();
+  sw.start();
+  sw.stop();
+  t.equal(sw.getState(), StopwatchState.IDLE);
+  t.equal(sw.isRunning(), false);
+  t.notEqual(sw.getDuration(), null);
+  t.end();
+});
+
+test(`reset`, (t) => {
+  const sw = new Stopwatch();
+  sw.start();
+  sw.reset();
+  t.equal(sw.getState(), StopwatchState.RUNNING);
+  sw.stop();
+  sw.reset();
+  t.equal(sw.isRunning(), false);
+  t.equal(sw.getDuration(), null);
+  t.equal(sw.getStartTime(), null);
+  t.end();
+});
+
+test(`getDuration()`, (t) => {
+  t.plan(3);
+  const sw = new Stopwatch();
+  sw.start();
+  setTimeout(() => {
+    t.equal(sw.isRunning(), true);
+    t.notEqual(sw.getStartTime(), null);
+    t.equal(sw.getDuration() > 50, true);
+    t.end();
+  }, 100);
+});
